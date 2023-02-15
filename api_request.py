34a45e274894fe3e8f706dd6c5a7659575dbff90
P3droPaulo <pedropaulo0624@gmail.com>
2023-02-15 18:46:51 -0300

    v2.2 beta

--- api_request.py
+++ api_request.py
@@ -9,12 +9,20 @@ class WeatherRequest():
         self.API_LINK = f'https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.API_KEY}&lang={self.lang}'
 
         self.request = requests.get(self.API_LINK).json()
+        
+        if self.request['cod'] == '404':
+            self.error = 404
+        else:
+            self.error = 202
+            self.celcius = int(self.request['main']['temp'] - 273.15)
+            self.description = self.request['weather'][0]['description']
+            self.humidity = self.request['main']['humidity']
+            self.pressure = self.request['main']['pressure']
+            self.feels_like = int(self.request['main']['feels_like'] - 273.15)
 
-        self.celcius = int(self.request['main']['temp'] - 273.15)
-        self.description = self.request['weather'][0]['description']
-        self.humidity = self.request['main']['humidity']
-        self.pressure = self.request['main']['pressure']
+            self.weatherData = [self.city, self.celcius, self.description, self.humidity, self.pressure,
+            self.feels_like]
 
-        self.weatherData = [self.city, self.celcius, self.description, self.humidity, self.pressure]
-
-print(WeatherRequest('Ouro Preto').request)
+if __name__ == "__main__":
+    app = WeatherRequest('Ouro Preto')
+    print(app.request)
--- app.py
+++ app.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    city_input = 'Conselheiro Pena'
+    city_input = 'Rio de Janeiro'
     weather = WeatherRequest(city_input)
 
-    return render_template('homepage.html', city=weather.weatherData[0], temp=weather.weatherData[1],
-    description=weather.weatherData[2], humidity=weather.weatherData[3], pressure=weather.weatherData[4])
+    if weather.error == 202:
+        return render_template('homepage.html', city=weather.weatherData[0], temp=weather.weatherData[1],
+        description=weather.weatherData[2], humidity=weather.weatherData[3], pressure=weather.weatherData[4],
+        feels_like=weather.weatherData[5])
+    elif weather.error == 404:
+        return render_template('not_found.html')
 
 def main():
     port = int(os.environ.get('PORT', 5000))
