34eec04f94f9d9f6c2b40565449b34df5ff0ba74
P3droPaulo <pedropaulo0624@gmail.com>
2023-02-23 09:50:49 -0300

    v5 beta

--- api_request.py
+++ api_request.py
@@ -6,6 +6,7 @@ class WeatherRequest():
         self.lang = lang
 
         self.API_KEY = '431184d81cc92171cee033a4fa4e305d'
+
         self.API_LINK = f'https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.API_KEY}&lang={self.lang}'
 
         self.request = requests.get(self.API_LINK).json()
@@ -15,16 +16,19 @@ class WeatherRequest():
         else:
             self.error = 202
             self.celcius = int(self.request['main']['temp'] - 273.15)
+            self.min = int(self.request['main']['temp_min'] - 273.15)
+            self.max = int(self.request['main']['temp_max'] - 273.15)
             self.description = self.request['weather'][0]['description']
             self.humidity = self.request['main']['humidity']
             self.pressure = self.request['main']['pressure']
             self.feels_like = int(self.request['main']['feels_like'] - 273.15)
             self.main = self.request['weather'][0]['main']
             self.visibility = int((self.request['visibility'] * 100) / 10000)
+            self.wind_speed = int(self.request['wind']['speed'] * 3.6)
 
-            self.data = {'city':self.city, 'celcius':self.celcius, 'description':self.description,
+            self.data = {'city':self.city, 'celcius':self.celcius, 'min':self.min, 'max':self.max, 'description':self.description,
             'humidity':self.humidity, 'pressure':self.pressure, 'feels_like':self.feels_like,
-            'main':self.main, 'visibility':self.visibility}
+            'main':self.main, 'visibility':self.visibility, 'wind_speed':self.wind_speed}
 
 if __name__ == "__main__":
     app = WeatherRequest('Ipatinga')
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from api_request import WeatherRequest
 import os
 
@@ -6,7 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    city_input = 'Ipatinga'
+    city_input = 'Belo Horizonte'
+    weatherRequest = WeatherRequest(city_input)
+
+    img = weatherRequest.data['main']
+    print(img)
+
+    if weatherRequest.error == 202:
+        return render_template('homepage.html', data=weatherRequest.data,
+        img=img)
+    elif weatherRequest.error == 404:
+        return render_template('not_found.html')
+
+@app.route('/weather', methods=["POST", "GET"])
+def buscar():
+    city_input = request.form['city_name']
     weatherRequest = WeatherRequest(city_input)
 
     img = weatherRequest.data['main']
